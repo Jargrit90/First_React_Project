151bc5440e650469d8088b5f12aede205e9622c8
Jaroslaw Grzybek <jargrit90.gh@gmail.com>
2022-10-23 23:57:59 +0200

    update links

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -14,23 +14,23 @@ function Menu(){
         <div>
             <div className="menu_buttons_container flexCC">
                 <div className="menu_button flexCC">
-                    <Link exact to="/" onClick={()=>dispatch({type: 'setIsVisibleFalse'})}>Strona główna</Link>
+                    <Link exact to="/First_React_Project/" onClick={()=>dispatch({type: 'setIsVisibleFalse'})}>Strona główna</Link>
                 </div>
                 <div className="menu_button flexCC">
-                    <Link to="/zespol" onClick={()=>dispatch({type: 'setIsVisibleFalse'})}>O zespole</Link>
+                    <Link to="/First_React_Project/zespol" onClick={()=>dispatch({type: 'setIsVisibleFalse'})}>O zespole</Link>
                 </div>
                 <div className="menu_button flexCC">
                     <div className="button_title" onClick={()=>dispatch({type: 'toggleState'})}>Członkinie zespołu</div>
                     {isVisible 
                     ?
                     <div className="band_members flexCC">
-                        <div className="band_member flexCC"><Link to="/czlonkinie/jiu" onClick={()=>{dispatch({type: 'changeMember', payload: 0}); dispatch({type: 'setIsVisibleFalse'})}}>JiU</Link></div>
-                        <div className="band_member flexCC"><Link to="/czlonkinie/sua" onClick={()=>{dispatch({type: 'changeMember', payload: 1}); dispatch({type: 'setIsVisibleFalse'})}}>SuA</Link></div>
-                        <div className="band_member flexCC"><Link to="/czlonkinie/siyeon" onClick={()=>{dispatch({type: 'changeMember', payload: 2}); dispatch({type: 'setIsVisibleFalse'})}}>Siyeon</Link></div>
-                        <div className="band_member flexCC"><Link to="/czlonkinie/handong" onClick={()=>{dispatch({type: 'changeMember', payload: 3}); dispatch({type: 'setIsVisibleFalse'})}}>Handong</Link></div>
-                        <div className="band_member flexCC"><Link to="/czlonkinie/yoohyeon" onClick={()=>{dispatch({type: 'changeMember', payload: 4}); dispatch({type: 'setIsVisibleFalse'})}}>Yoohyeon</Link></div>
-                        <div className="band_member flexCC"><Link to="/czlonkinie/dami" onClick={()=>{dispatch({type: 'changeMember', payload: 5}); dispatch({type: 'setIsVisibleFalse'})}}>Dami</Link></div>
-                        <div className="band_member flexCC"><Link to="/czlonkinie/gahyeon" onClick={()=>{dispatch({type: 'changeMember', payload: 6}); dispatch({type: 'setIsVisibleFalse'})}}>Gahyeon</Link></div>
+                        <div className="band_member flexCC"><Link to="/First_React_Project/czlonkinie/jiu" onClick={()=>{dispatch({type: 'changeMember', payload: 0}); dispatch({type: 'setIsVisibleFalse'})}}>JiU</Link></div>
+                        <div className="band_member flexCC"><Link to="/First_React_Project/czlonkinie/sua" onClick={()=>{dispatch({type: 'changeMember', payload: 1}); dispatch({type: 'setIsVisibleFalse'})}}>SuA</Link></div>
+                        <div className="band_member flexCC"><Link to="/First_React_Project/czlonkinie/siyeon" onClick={()=>{dispatch({type: 'changeMember', payload: 2}); dispatch({type: 'setIsVisibleFalse'})}}>Siyeon</Link></div>
+                        <div className="band_member flexCC"><Link to="/First_React_Project/czlonkinie/handong" onClick={()=>{dispatch({type: 'changeMember', payload: 3}); dispatch({type: 'setIsVisibleFalse'})}}>Handong</Link></div>
+                        <div className="band_member flexCC"><Link to="/First_React_Project/czlonkinie/yoohyeon" onClick={()=>{dispatch({type: 'changeMember', payload: 4}); dispatch({type: 'setIsVisibleFalse'})}}>Yoohyeon</Link></div>
+                        <div className="band_member flexCC"><Link to="/First_React_Project/czlonkinie/dami" onClick={()=>{dispatch({type: 'changeMember', payload: 5}); dispatch({type: 'setIsVisibleFalse'})}}>Dami</Link></div>
+                        <div className="band_member flexCC"><Link to="/First_React_Project/czlonkinie/gahyeon" onClick={()=>{dispatch({type: 'changeMember', payload: 6}); dispatch({type: 'setIsVisibleFalse'})}}>Gahyeon</Link></div>
                     </div>
                     :
                     null
